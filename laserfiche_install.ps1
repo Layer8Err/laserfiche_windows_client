94c8dab8bf72f99cd0f219674238897b589b8041
Dwight Brenner <d.brenner@truepointwealth.com>
2021-10-14 13:55:29 -0400

    New function to handle LF upgrades

--- laserfiche_install.ps1
+++ laserfiche_install.ps1
@@ -215,12 +215,129 @@ function Install-Laserfiche () {
     Install-SetupLf
 }
 
+function Update-Laserfiche () {
+    # Update Laserfiche
+    Param (
+        [Parameter(Mandatory = $true)] [string]$InstallerRoot,
+        [ValidateNotNullOrEmpty()] $LFreqs
+    )
+    $installedSoftware = ( Get-Package -Provider Programs -IncludeWindowsInstaller | Select-Object * )
+
+    function Wait-Msiexec ($MaxWait=300) {
+        $startTime = Get-Date
+        $forceEndTime = (Get-Date).AddSeconds($MaxWait)
+        while (((Get-Process -Name msiexec -ErrorAction:SilentlyContinue).Length -ne 0) -and ((Get-Date) -lt $forceEndTime)){
+            Start-Sleep -Seconds 5 # Backoff 5 seconds if an installer is still running
+        }
+        if ((Get-Process -Name msiexec -ErrorAction:SilentlyContinue).Length -ne 0) {
+            # Assume that msiexec.exe is crashed/hung
+            # Write-Warning "Waiting since $startTime, but MaxWait exceeded $MaxWait seconds. Terminating msiexec."
+            Get-Process -Name msiexec | Stop-Process -Force
+        }
+    }
+    function Install-PreReqs () {
+        # Install programs required by Laserfiche
+        function Check-Installed ($Name) {
+            # Check if a specific software package is already installed
+            $newName = $Name
+            if ($Name -match '\++'){
+                $newName = $Name.Replace('++', '\++')
+            }
+            $isInstalled = $false
+            $installedSoftware | ForEach-Object {
+                $checkName = $_.Name
+                if ($checkName -match $newName) {
+                    $isInstalled = $true
+                }
+            }
+            return $isInstalled
+        }
+        
+        # MSXML 6.0 should be in $env:WINDIR\System32\Msxml6.dll
+        # if (Test-Path "$InstallerRoot\Support\msxml6_x86.msi"){
+        #     Write-Host " * Installing MSXML 6.0 Parser (x86) SP1..."
+        #     msiexec.exe /i ($InstallerRoot + "\Support\msxml6_x86.msi") /qn
+        # }
+        if (Test-Path "$InstallerRoot\Support\msxml6_x64.msi"){
+            if (!(Test-Path "$env:WINDIR\System32\msxml6.dll")){
+                Write-Host " * Installing MSXML 6.0 Parser (x64) SP1..."
+                Wait-Msiexec -MaxWait 5
+                msiexec.exe /i ($InstallerRoot + "\Support\msxml6_x64.msi") /qn
+            }
+        }
+        if (!(Check-Installed -Name 'Microsoft Visual C++ 2015-2019 Redistributable (x86)*')){
+            Write-Host " * Installing Microsoft Visual C++ 2019 Redistributable (x86) - 14.28.29913.0..."
+            Start-Process -FilePath ($InstallerRoot + "\Support\MSVC2019\VC_redist.x86.exe") -ArgumentList "/install /quiet /norestart" -Wait
+        }
+        if (!(Check-Installed -Name 'Microsoft Visual C++ 2015-2019 Redistributable (x64)*')){
+            Write-Host " * Installing Microsoft Visual C++ 2019 Redistributable (x64) - 14.28.29913.0..."
+            Start-Process -FilePath ($InstallerRoot + "\Support\MSVC2019\VC_redist.x64.exe") -ArgumentList "/install /quiet /norestart" -Wait
+        }
+        if (!(Check-Installed -Name 'Microsoft Edge WebView2 Runtime')){
+            Write-Host " * Installing Microsoft Edge Web View 2 Runtime (x64)..."
+            Start-Process -FilePath ($InstallerRoot + "\Support\MicrosoftEdgeWebView2RuntimeInstallerX64.exe") -ArgumentList "/silent /install" -Wait
+        }
+    }
+    function Install-SetupLf () {
+        # Use SetupLf.exe to install Laserfiche
+        $logFolder = $InstallerRoot + "\LFInstall_Log"
+        $instArgs = "/silent /-noui /-iacceptlicenseagreement -log $logFolder INSTALLLEVEL=300"
+        $killList = @(
+            'Laserfiche.OfficeMonitor',
+            'Laserfiche Webtools Agent',
+            'SetupLf',
+            'msiexec',
+            'EXCEL',
+            'WINWORD',
+            'POWERPNT'
+        ) # Programs that cannot be running while installing Laserfiche
+        $installerPath = $InstallerRoot + "\ClientWeb\SetupLf.exe"
+        if (Test-Path $installerPath){
+            $killList | ForEach-Object {
+                Stop-Process -Name $_ -Force -ErrorAction:SilentlyContinue
+            }
+            $instWorkingDir = $InstallerRoot + '\ClientWeb'
+            Start-Process -FilePath $installerPath -ArgumentList $instArgs -WorkingDirectory $instWorkingDir -NoNewWindow -Wait
+        } else {
+            Write-Error "SetupLf.exe could not be found at $installerPath"
+        }
+    }
+    function Update-Lf () {
+        # Use lfoffice-x64_en.msi and lfwebtools.msi to update an existing Laserfiche install
+        $killList = @(
+            'Laserfiche.OfficeMonitor',
+            'Laserfiche Webtools Agent',
+            'SetupLf',
+            'msiexec',
+            'EXCEL',
+            'WINWORD',
+            'POWERPNT'
+        ) # Programs that cannot be running while installing Laserfiche
+        $lfoffice = $InstallerRoot + "\ClientWeb\lfoffice-x64_en.msi"
+        $lfwebtools = $InstallerRoot + "\ClientWeb\lfwebtools.msi"
+        if ((Test-Path $lfoffice) -and (Test-Path $lfwebtools)){
+            $lfofficeArgs = 'REBOOT=ReallySuppress INSTALLDIR="' + $env:ProgramFiles + '\Laserfiche\Client\" INSTALLDIR32="' + ${env:ProgramFiles(x86)} + '\Laserfiche\Client\" ADDLOCAL="LFOffice" REINSTALL=ALL REINSTALLMODE=vomus'
+            $lfwebtoolsArgs = 'REBOOT=ReallySuppress INSTALLDIR="' + ${env:ProgramFiles(x86)} + '\Laserfiche\Webtools Agent\" ADDLOCAL="LFWebtools" REINSTALL=ALL REINSTALLMODE=vomus'
+            msiexec.exe /i $lfoffice /qn $lfofficeArgs
+            Wait-Msiexec
+            msiexec.exe /i $lfwebtools /qn $lfwebtoolsArgs
+        } else {
+            Write-Error "Could not find files required to update Laserfiche"
+        }
+    }
+    Write-Host "Installing Laserfiche..."
+    Install-PreReqs
+    Wait-Msiexec # Wait for any running install processes to finish
+    # Install-SetupLf
+    Update-Lf
+}
+
 $LFInfo = Check-LFRequired
 
 if ($LFInfo.Install_Webtools -or $LFInfo.Install_OfficeIntegration){
     Write-Host "Laserfiche is not installed... Beginning Install process..."
     if (Test-Path $LFTempRoot){
-        Remove-Item -Recurse -Path $LFTempRoot -Force
+        Remove-Item -Recurse -Path $LFTempRoot -Force -ErrorAction:SilentlyContinue
         New-Item -Path $LFTempRoot -ItemType Directory | Out-Null
     } else {
         New-Item -Path $LFTempRoot -ItemType Directory | Out-Null
@@ -231,6 +348,12 @@ if ($LFInfo.Install_Webtools -or $LFInfo.Install_OfficeIntegration){
 } elseif ($LFInfo.Upgrade_OfficeIntegration -or $LFInfo.Upgrade_Webtools){
     Write-Host "Laserfiche is not current version.. Beginning Update process..."
     # TODO: If SetupLf.exe detects that this is a minor, upgrade nothing happens
+    if (Test-Path $LFTempRoot){
+        Remove-Item -Recurse -Path $LFTempRoot -Force -ErrorAction:SilentlyContinue
+        New-Item -Path $LFTempRoot -ItemType Directory | Out-Null
+    } else {
+        New-Item -Path $LFTempRoot -ItemType Directory | Out-Null
+    }
     if ($LFInfo.Upgrade_OfficeIntegration){
         Write-Host "Current Laserfiche Office Integration version: $($LFInfo.Current_OfficeIntegration_Ver) [version $($LFInfo.Latest_OfficeIntegration_Ver) available]"
     }
@@ -239,7 +362,7 @@ if ($LFInfo.Install_Webtools -or $LFInfo.Install_OfficeIntegration){
     }
     Download-Laserfiche -Path $LFTempRoot
     Extract-Laserfiche -Installer "$LFTempRoot\$LFInstallerName" -Path $LFTempRoot
-    Install-Laserfiche -InstallerRoot $LFTempRoot -LFreqs $LFInfo # TODO: Make sure that this updates an old version of Laserfiche
+    Update-Laserfiche -InstallerRoot $LFTempRoot -LFreqs $LFInfo # TODO: Make sure that this updates an old version of Laserfiche
 } else {
     Write-Host "Laserfiche appears to be installed and up-to-date"
     Write-Host "Installed Laserfiche Office Integration version: $($LFInfo.Current_OfficeIntegration_Ver) [latest version $($LFInfo.Latest_OfficeIntegration_Ver)]"
